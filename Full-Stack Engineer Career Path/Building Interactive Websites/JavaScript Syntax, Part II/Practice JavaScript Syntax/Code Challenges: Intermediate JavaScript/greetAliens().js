0f5c05b7cb677de741ed7c23da7db5d06c3a39c4
Bruceiplol <128085760+Bruceiplol@users.noreply.github.com>
2023-05-28 13:06:47 +0100

    Create greetAliens().js

---
+++ Full-Stack Engineer Career Path/Building Interactive Websites/JavaScript Syntax, Part II/Practice JavaScript Syntax/Code Challenges: Intermediate JavaScript/greetAliens().js
@@ -0,0 +1,16 @@
+// Write your code here:
+const greetAliens = (aliens) => {
+  for (let alien of aliens) {
+    console.log(`Oh powerful ${alien}, we humans offer our unconditional surrender!`)
+  }
+  return
+}
+
+
+
+// When you're ready to test your code, uncomment the below and run:
+
+const aliens = ["Blorgous", "Glamyx", "Wegord", "SpaceKing"];
+
+greetAliens(aliens);
+
