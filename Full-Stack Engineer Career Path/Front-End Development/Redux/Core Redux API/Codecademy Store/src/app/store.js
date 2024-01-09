f3705584ae5ca3078eaa7e93028f6e8d9869d791
Bruceiplol <128085760+Bruceiplol@users.noreply.github.com>
2024-01-10 00:24:08 +0800

    Update store.js

--- Full-Stack Engineer Career Path/Front-End Development/Redux/Core Redux API/Codecademy Store/src/app/store.js
+++ Full-Stack Engineer Career Path/Front-End Development/Redux/Core Redux API/Codecademy Store/src/app/store.js
@@ -4,12 +4,14 @@ import {createStore, combineReducers} from 'redux'
 import { inventoryReducer } from '../features/inventory/inventorySlice.js'
 import { cartReducer } from '../features/cart/cartSlice.js'
 import { currencyFilterReducer } from '../features/currencyFilter/currencyFilterSlice.js'
+import { searchTermReducer } from '../features/searchTerm/searchTermSlice.js'
 
 // Create and export the store here.
 const reducers = {
   inventory: inventoryReducer,
   cart: cartReducer,
-  currencyFilter: currencyFilterReducer
+  currencyFilter: currencyFilterReducer,
+  searchTerm: searchTermReducer
 }
 
 const rootReducer = combineReducers(reducers);
