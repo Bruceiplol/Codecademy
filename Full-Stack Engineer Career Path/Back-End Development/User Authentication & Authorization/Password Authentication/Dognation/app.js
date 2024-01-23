0c20082d3863c8c836ee980651b1fd855c94a69f
Bruceiplol <128085760+Bruceiplol@users.noreply.github.com>
2024-01-23 14:11:07 +0800

    Update app.js

--- Full-Stack Engineer Career Path/Back-End Development/User Authentication & Authorization/Password Authentication/Dognation/app.js
+++ Full-Stack Engineer Career Path/Back-End Development/User Authentication & Authorization/Password Authentication/Dognation/app.js
@@ -19,7 +19,7 @@ app.use(
   session({
     secret: "D53gxl41G",
     cookie: { maxAge: 1000 * 60 * 60 * 24 },
-    saveUnintialized: false,
+    saveUninitialized: false,
     resave: false,
     sameSite: "none",
     secure: true,
